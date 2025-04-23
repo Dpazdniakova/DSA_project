b4c1e6946f40b12aa2006da84ebdb20cb15046e9
Dpazdniakova <20107671@mail.wit.ie>
2025-04-23 15:15:50 +0100

    Button selection UI added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -29,23 +29,54 @@ fun App() {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = "Menu",
-                        style = MaterialTheme.typography.headlineSmall,
-                        color = Color.White
-                    ) },
+                    Text("Menu", style = MaterialTheme.typography.headlineSmall, color = Color.White)
+                },
                 actions = {
-                    MenuButton(Icons.Filled.FormatListNumbered, "Biggest number") { selectedMenuItem = MenuItem.Numbers }
-                    Spacer(modifier = Modifier.width(width = 16.dp))
-                    MenuButton(Icons.Filled.Abc, "Characters count") { selectedMenuItem = MenuItem.Vowels }
-                    Spacer(modifier = Modifier.width(width = 16.dp))
-                    MenuButton(Icons.Filled.Abc, "Palindrome") { selectedMenuItem = MenuItem.Palindrome }
-                    Spacer(modifier = Modifier.width(width = 16.dp))
-                    MenuButton(Icons.Filled.Equalizer, "Frequency") { selectedMenuItem = MenuItem.FrequentString }
-                    Spacer(modifier = Modifier.width(width = 16.dp))
-                    MenuButton(Icons.Filled.Equalizer, "Needle in Haystack") { selectedMenuItem = MenuItem.countOccurrences }
-                    Spacer(modifier = Modifier.width(width = 16.dp))
+                    MenuButton(
+                        icon = Icons.Filled.FormatListNumbered,
+                        label = "Biggest number",
+                        selected = selectedMenuItem == MenuItem.Numbers,
+                        onClick = { selectedMenuItem = MenuItem.Numbers }
+                    )
+
+                    Spacer(Modifier.width(8.dp))
+
+                    MenuButton(
+                        icon = Icons.Filled.Abc,
+                        label = "Characters count",
+                        selected = selectedMenuItem == MenuItem.Vowels,
+                        onClick = { selectedMenuItem = MenuItem.Vowels }
+                    )
+
+                    Spacer(Modifier.width(8.dp))
+
+                    MenuButton(
+                        icon = Icons.Filled.Abc,
+                        label = "Palindrome",
+                        selected = selectedMenuItem == MenuItem.Palindrome,
+                        onClick = { selectedMenuItem = MenuItem.Palindrome }
+                    )
+
+                    Spacer(Modifier.width(8.dp))
+
+                    MenuButton(
+                        icon = Icons.Filled.Equalizer,
+                        label = "Frequency",
+                        selected = selectedMenuItem == MenuItem.FrequentString,
+                        onClick = { selectedMenuItem = MenuItem.FrequentString }
+                    )
+
+                    Spacer(Modifier.width(8.dp))
+
+                    MenuButton(
+                        icon = Icons.Filled.Equalizer,
+                        label = "Needle in Haystack",
+                        selected = selectedMenuItem == MenuItem.countOccurrences,
+                        onClick = { selectedMenuItem = MenuItem.countOccurrences }
+                    )
                 }
             )
+
         },
         content = {
             when (selectedMenuItem) {
--- src/main/kotlin/ui/components/MenuButton.kt
+++ src/main/kotlin/ui/components/MenuButton.kt
@@ -2,7 +2,6 @@ package ui.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
@@ -11,16 +10,23 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.vector.ImageVector
 
 
 @Composable
-fun MenuButton(icon: ImageVector, label: String, onClick: () -> Unit) {
-    IconButton(onClick = onClick) {
-        Row {
-            Icon(imageVector = icon, contentDescription = label)
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(label, color = Color.White, modifier = Modifier.padding(top = 2.dp))
+fun MenuButton(icon: ImageVector, label: String,  selected: Boolean, onClick: () -> Unit) {
+
+    val HighlightColor = Color(0xFFFF7043)
+    val textColor = if (selected) HighlightColor else Color.White
+
+
+        IconButton(onClick = onClick) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(icon, contentDescription = label)
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(label, color = textColor)
+            }
         }
-    }
+
 }
